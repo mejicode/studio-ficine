0737d99d31fc06618568ece50a40ea3285e5fc9c
Daniela Barbosab <danielalfbarbosa@gmail.com>
2026-03-19 00:47:21 +0000

    align equipe

--- schemaTypes/teamType.ts
+++ schemaTypes/teamType.ts
@@ -1,51 +1,59 @@
 import {defineField, defineType} from 'sanity'
 
 export const teamType = defineType({
-    name: 'team',
+  name: 'team',
   title: 'Equipe',
   type: 'document',
   fields: [
     defineField({
-        name: 'name',
-        title: 'Nome',
-        type: 'string',
-        validation: (rule) => rule.required(),
-      }),
+      name: 'name',
+      title: 'Nome',
+      type: 'string',
+      validation: (rule) => rule.required(),
+    }),
     defineField({
-        name: 'role',
-        title: 'LinkTree',
-        type: 'string',
-      }),
+      name: 'role',
+      title: 'Cargo',
+      type: 'string',
+    }),
     defineField({
-        name: 'bio',
-        title: 'Bio em PT',
-        type: 'string',
-      }),
-      defineField({
-        name: 'bioEng',
-        title: 'Bio em EN',
-        type: 'string',
-      }),
-      defineField({
-        name: 'bioEs',
-        title: 'Bio em ES',
-        type: 'string',
-      }),
-      defineField({
-        name: 'image',
-        type: 'image',
-      }),
-      defineField({
-        name: 'category',
-        title: 'Categoria',
-        type: 'array',
-        of: [
-          {
-            type: 'reference',
-            to: [{type: 'category'}],
-          },
-        ],
-        validation: (rule) => rule.required(),
-      }),
-  ]
+      name: 'link',
+      title: 'Link',
+      type: 'string',
+    }),
+    defineField({
+      name: 'bio',
+      title: 'Bio em PT',
+      type: 'text',
+      rows: 4,
+    }),
+    defineField({
+      name: 'bioEng',
+      title: 'Bio em EN',
+      type: 'text',
+      rows: 4,
+    }),
+    defineField({
+      name: 'bioEs',
+      title: 'Bio em ES',
+      type: 'text',
+      rows: 4,
+    }),
+    defineField({
+      name: 'image',
+      type: 'image',
+      options: {hotspot: true},
+    }),
+    defineField({
+      name: 'category',
+      title: 'Categoria',
+      type: 'array',
+      of: [
+        {
+          type: 'reference',
+          to: [{type: 'category'}],
+        },
+      ],
+    }),
+  ],
 })
